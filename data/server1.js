c9a5fd629e816b957204a75140ff522fef6f0062
Kenneth Whitebloom <kenwhi@vagelos-ve505-0419.apn.wlan.upenn.edu>
2019-02-25 11:01:17 -0500

    Add functional bibliographic extraction.

--- data/server1.js
+++ data/server1.js
@@ -1,9 +1,13 @@
+'use strict';
+
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const URL = 'http://sceti.library.upenn.edu/Philaneighborhoods/browse.cfm#';
 
 let scrape = async () => {
+    let dataObj = [];
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
@@ -21,8 +25,9 @@ let scrape = async () => {
     });
 
     // Generate an array of objects with all of the top-level links to our content (main page, table of contents, bibliographic info, and the image start).
+    console.log('Parsing ids and creating links..');
     let links = [];
-    for (i = 0; i < ids.length; i++) {
+    for (let i = 0; i < ids.length; i++) {
         let id = `${ids[i]}`; // Parse integers?
         let main = `http://sceti.library.upenn.edu/pages/index.cfm?so_id=${ids[i]}`;
         let toc = `http://sceti.library.upenn.edu/pages/popup_jump.cfm?so_id=${ids[i]}&pageposition=1`;
@@ -45,7 +50,7 @@ let scrape = async () => {
 
     // NEXT STEPS: 
     // Navigate to each of the Table of Contents and extract the page numbers and headings, then write to a file appropriately title folder;
-    // let testToc = [ links[0].toc, links[1].toc ];
+    console.log('Extracting table of contents...');
     let extractedTocData = [];
 
     for (let i = 0; i < links.length; i++) {
@@ -55,7 +60,7 @@ let scrape = async () => {
         const tocs = await page.evaluate(() => {
             let sections = document.querySelector('body > table > tbody > tr:nth-child(1) > td:nth-child(2) > select');
 
-            data = [];
+            let data = [];
             for (let i = 0; i < sections.length; i++) {
                 let pageTitle = sections[i].innerText.trim();
                 let pageNumber = parseInt(sections[i].getAttribute('value'));
@@ -68,26 +73,60 @@ let scrape = async () => {
             return data;
         }); 
 
-        extractedTocData.push({
+        dataObj.push({
             report: links[i],
             tocs,
         });
     }
 
-    fs.writeFile('./outputs/toc-data.json', JSON.stringify(extractedTocData, null, 2), (err) => {
+    // Write our TOC to a JSON file.
+    // fs.writeFile('./outputs/toc-data2.json', JSON.stringify(dataObj, null, 2), (err) => {
+    //     console.log(err);
+    // });
+
+    // Navigate to each of the bibliographic info pages and extract all of the relevant metadata, then write to a file appropriately title folder;
+    console.log('Extracting bibliographic data...');
+    for (let i = 0; i < dataObj.length; i++) {
+        const url = dataObj[i].report.bibInfo;
+        await page.goto(url);
+        // Evaluate each page and extract relevant bibliographic data.
+        const bibData = await page.evaluate(() => {
+            let content = document.querySelectorAll('td.DC_content');
+
+            let title = content[0].innerText.trim();
+            let creator = content[1].innerText.trim();
+            let callNumber = content[2].innerText.trim();
+            let uriRecord = content[3].innerText.trim();
+            let uriFacsimile = content[4].innerText.trim();
+
+            let data = {
+                title,
+                creator,
+                callNumber,
+                uriRecord,
+                uriFacsimile
+            }
+            return data;
+        }); 
+
+        // Add our bibliographic data to each object containing our report data.
+        dataObj[i].bibliographicData = bibData;
+
+    }
+
+    fs.writeFile('./outputs/toc-data3.json', JSON.stringify(dataObj, null, 2), (err) => {
         console.log(err);
     });
 
-    // Navigate to each of the bibliographic info pages and extract all of the relevant metadata, then write to a file appropriately title folder;
     // Navigate to each image page based on the number of pages and save jpegs to places.
 
     // End our browser session.
     browser.close();
 
-    // Return whatever needs returning.
-    return extractedTocData;
+    // Return our object of data.
+    return dataObj;
 };
 
 scrape().then((value) => {
-    console.log(value); // Success!
+    console.log("Success! All scraped :)"); // Success!
 });
