351a1fe1f304b03f84573764eb7ec9ae5c0d436f
calemonte <29611438+calemonte@users.noreply.github.com>
2019-03-12 17:09:21 -0400

    Fix bug in updating filename script

--- data/updatefilname.js
+++ data/updatefilname.js
@@ -4,31 +4,38 @@
 
     Example: 2601page1.jpg --> 2601page001.jpg
 
-    This was needed in order to effeciently process jps to pdf using ImageMagick.
+    This was needed in order to effeciently process jps to pdf using ImageMagick. You
 */
 
 const fs = require("fs");
 
-const files = fs.readdir("./outputs/images", (error, files) => {
+fs.readdir("./test", (error, files) => {
   try {
     for (let i = 0; i < files.length; i++) {
-      // If the file name is 13 characters long, then it has to be pages 1-9.
-      if (files[i].length === 13 && files[i].includes("page")) {
-        const endOfFile = files[i].match(/(\d).jpg/);
-        const pageNumber = endOfFile[1];
-
+      if (files[i].includes("page")) {
         const splitFile = files[i].split("page");
         const reportId = splitFile[0];
+        const splitExtension = splitFile[1].split(".");
+        const pageNumber = splitExtension[0];
 
-        const newFile = `${reportId}page00${pageNumber}.jpg`;
+        // If the page number is 1 character, then it has to be pages 1-9.
+        if (pageNumber.length === 1) {
+          const newFile = `${reportId}page00${pageNumber}.jpg`;
+          
+          fs.rename(`./test/${files[i]}`, `./test/${newFile}`, err => {
+            if (err) console.log(err);
+          });
+        }
 
-        fs.rename(`./outputs/images/${files[i]}`, `./outputs/images/${newFile}`, err => {
-          if (err) console.log(err);
-        });
-
-        // console.log(endOfFile[1]);
-        // console.log(splitFile);
-        // console.log(newFile);
+        // If the file name is 2 characters long, then it has to be pages 10-99.
+        if (pageNumber.length === 2) {
+          const newFile = `${reportId}page0${pageNumber}.jpg`;
+          
+          fs.rename(`./test/${files[i]}`, `./test/${newFile}`, err => {
+            if (err) console.log(err);
+          });
+        }
+        
       }
     }
   } catch (error) {
