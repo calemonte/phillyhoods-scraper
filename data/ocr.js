3e77753704c68594c000ff50b817c7a23be489e4
calemonte <29611438+calemonte@users.noreply.github.com>
2019-03-13 16:36:55 -0400

    Add script for turning jpgs to tiffs and then performing OCR on the tiffs.

---
+++ data/ocr.js
@@ -0,0 +1,48 @@
+const shell = require("shelljs");
+const fs = require("fs");
+
+fs.readdir("./outputs/images", (error, files) => {
+  try {
+    let reports = [];
+
+    // Loop over our files and extract the unique ids.
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].includes("page")) {
+        const splitFile = files[i].split("page");
+        const reportID = splitFile[0];
+
+        if (!reports.includes(reportID)) {
+          reports.push(reportID);
+        }
+      }
+    }
+
+    console.log("Done mapping directory!");
+
+    // Loop over our array of unique ids and convert jpgs into tiffs for processing by tesseract.
+    for (let j = 0; j < reports.length; j++) {
+
+      if (shell.which("convert")) {
+        shell.exec(
+          `convert -density 300 ./outputs/images/${
+            reports[j]
+          }page*.jpg -depth 8 -strip -background white -alpha off ./outputs/ocr/${
+            reports[j]
+          }ocr.tiff`
+        );
+      }
+      console.log(`TIFFitized ${reports[j]}`);
+
+      // Perform ocr on the newly minted tiff file.
+      if (shell.which("tesseract")) {
+        shell.exec(`tesseract ./outputs/ocr/${reports[j]}ocr.tiff ./outputs/ocr/${reports[j]}`);
+      }
+      console.log(`OCR'd ${reports[j]}`);
+      
+    }
+  } catch (error) {
+    console.log("Something went wrong with the conversion and OCR process: " + error);
+  }
+
+  console.log("File Read Error: " + error);
+});
