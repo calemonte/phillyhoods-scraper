7de5b1b89510e7145fd94d56a03ed6f0c205d7bb
calemonte <29611438+calemonte@users.noreply.github.com>
2019-03-19 12:06:42 -0400

    Update writecsv to include reportID

--- data/writecsv.js
+++ data/writecsv.js
@@ -1,11 +1,12 @@
 const reports = require("./outputs/hoodsDataWithRegions.json");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const csvWriter = createCsvWriter({
-  path: "outputs/PhillyHoodsRecords.csv",
+  path: "outputs/PhillyHoodsRecords_withReportIds.csv",
   header: [
     { id: "title", title: "TITLE" },
     { id: "year", title: "YEAR" },
-    { id: "callNumber", title: "CALL NUMBER" }
+    { id: "callNumber", title: "CALL NUMBER" },
+    { id: "reportId", title: "REPORT ID" },
   ]
 });
 
@@ -14,7 +15,8 @@ for (let i = 0; i < reports.length; i++) {
   const oneRecord = {
     title: `${reports[i].bibliographicData.title}`,
     year: `${reports[i].bibliographicData.year}`,
-    callNumber: `${reports[i].bibliographicData.callNumber}`
+    callNumber: `${reports[i].bibliographicData.callNumber}`,
+    reportId: `${reports[i].report.id}`
   };
   records.push(oneRecord);
 }
